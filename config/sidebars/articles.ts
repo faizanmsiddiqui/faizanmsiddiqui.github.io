93ea862aec8d4e9f0bab90873ddcd5547a457f6d
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-07-29 16:37:46 +0300

    ✨ feat(sidebars): add articles and notes sidebars configuration

---
+++ config/sidebars/articles.ts
@@ -0,0 +1,18 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const articles: SidebarsConfig = {
+  seriesSidebar: [
+    {
+      type: "category",
+      label: "Articles",
+      link: {
+        type: "doc",
+        id: "index",
+      },
+      items: [
+      ],
+    },
+  ],
+};
+
+export default articles;
---
+++ config/sidebars/notes.ts
@@ -0,0 +1,19 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const notes: SidebarsConfig = {
+  seriesSidebar: [
+    {
+      type: "category",
+      label: "Notes",
+      link: {
+        type: "doc",
+        id: "index",
+      },
+      items: [
+
+      ],
+    },
+  ],
+};
+
+export default notes;
