844298d05eae79750f7143ee55aaebf971c6ca43
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-08-11 18:48:50 +0300

    ✨ feat(assets): export wrapped PNG components for emojis
    Added wrapped PNG exports for various emoji assets, including Books,
    Cloud, CrystalBall, GameDie, Herb, Memo, PageFacingUp, Penguin,
    Rocket, Seal, Snake, SpiralShell, SpoutingWhale, and Whale. This
    enhancement allows for easier integration of these assets in React
    components, improving the overall asset management in the project.

--- src/assets/noto-emoji/index.tsx
+++ src/assets/noto-emoji/index.tsx
@@ -1,3 +1,20 @@
+import wrapPng from "@site/src/assets";
+
+import BooksPng from "./emoji_u1f4da.png";
+import CloudPng from "./emoji_u2601.png";
+import CrystalBallPng from "./emoji_u1f52e.png";
+import GameDiePng from "./emoji_u1f3b2.png";
+import HerbPng from "./emoji_u1f33f.png";
+import MemoPng from "./emoji_u1f4dd.png";
+import PageFacingUpPng from "./emoji_u1f4c4.png";
+import PenguinPng from "./emoji_u1f427.png";
+import RocketPng from "./emoji_u1f680.png";
+import SealPng from "./emoji_u1f9ad.png";
+import SnakePng from "./emoji_u1f40d.png";
+import SpiralShellPng from "./emoji_u1f41a.png";
+import SpoutingWhalePng from "./emoji_u1f433.png";
+import WhalePng from "./emoji_u1f40b.png";
+
 import Books from "./emoji_u1f4da.svg";
 import Cloud from "./emoji_u2601.svg";
 import CrystalBall from "./emoji_u1f52e.svg";
@@ -12,6 +29,22 @@ import Snake from "./emoji_u1f40d.svg";
 import SpiralShell from "./emoji_u1f41a.svg";
 import SpoutingWhale from "./emoji_u1f433.svg";
 import Whale from "./emoji_u1f40b.svg";
+
+// Export wrapped components
+export const Books512 = wrapPng(BooksPng, 'Books512');
+export const Cloud512 = wrapPng(CloudPng, 'Cloud512');
+export const CrystalBall512 = wrapPng(CrystalBallPng, 'CrystalBall512');
+export const GameDie512 = wrapPng(GameDiePng, 'GameDie512');
+export const Herb512 = wrapPng(HerbPng, 'Herb512');
+export const Memo512 = wrapPng(MemoPng, 'Memo512');
+export const PageFacingUp512 = wrapPng(PageFacingUpPng, 'PageFacingUp512');
+export const Penguin512 = wrapPng(PenguinPng, 'Penguin512');
+export const Rocket512 = wrapPng(RocketPng, 'Rocket512');
+export const Seal512 = wrapPng(SealPng, 'Seal512');
+export const Snake512 = wrapPng(SnakePng, 'Snake512');
+export const SpiralShell512 = wrapPng(SpiralShellPng, 'SpiralShell512');
+export const SpoutingWhale512 = wrapPng(SpoutingWhalePng, 'SpoutingWhale512');
+export const Whale512 = wrapPng(WhalePng, 'Whale512');
 
 export {
   Books,
