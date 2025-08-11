15de154619ebd243d6e959e4170cd4bf4ac79585
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-08-11 18:40:08 +0300

    ✨ feat(assets): export wrapped 3D emoji components

--- src/assets/fluentui-emoji/index.tsx
+++ src/assets/fluentui-emoji/index.tsx
@@ -1,3 +1,20 @@
+import wrapPng from "@site/src/assets";
+
+import Books3dPng from "./books_3d.png";
+import Cloud3dPng from "./cloud_3d.png";
+import CrystalBall3dPng from "./crystal_ball_3d.png";
+import GameDie3dPng from "./game_die_3d.png";
+import Herb3dPng from "./herb_3d.png";
+import Memo3dPng from "./memo_3d.png";
+import PageFacingUp3dPng from "./page_facing_up_3d.png";
+import Penguin3dPng from "./penguin_3d.png";
+import Rocket3dPng from "./rocket_3d.png";
+import Seal3dPng from "./seal_3d.png";
+import Snake3dPng from "./snake_3d.png";
+import SpiralShell3dPng from "./spiral_shell_3d.png";
+import SpoutingWhale3dPng from "./spouting_whale_3d.png";
+import Whale3dPng from "./whale_3d.png";
+
 import BooksColor from "./books_color.svg";
 import CloudColor from "./cloud_color.svg";
 import CrystalBallColor from "./crystal_ball_color.svg";
@@ -12,6 +29,22 @@ import SnakeColor from "./snake_color.svg";
 import SpiralShellColor from "./spiral_shell_color.svg";
 import SpoutingWhaleColor from "./spouting_whale_color.svg";
 import WhaleColor from "./whale_color.svg";
+
+// Export wrapped components
+export const Books3d = wrapPng(Books3dPng, 'Books3d');
+export const Cloud3d = wrapPng(Cloud3dPng, 'Cloud3d');
+export const CrystalBall3d = wrapPng(CrystalBall3dPng, 'CrystalBall3d');
+export const GameDie3d = wrapPng(GameDie3dPng, 'GameDie3d');
+export const Herb3d = wrapPng(Herb3dPng, 'Herb3d');
+export const Memo3d = wrapPng(Memo3dPng, 'Memo3d');
+export const PageFacingUp3d = wrapPng(PageFacingUp3dPng, 'PageFacingUp3d');
+export const Penguin3d = wrapPng(Penguin3dPng, 'Penguin3d');
+export const Rocket3d = wrapPng(Rocket3dPng, 'Rocket3d');
+export const Seal3d = wrapPng(Seal3dPng, 'Seal3d');
+export const Snake3d = wrapPng(Snake3dPng, 'Snake3d');
+export const SpiralShell3d = wrapPng(SpiralShell3dPng, 'SpiralShell3d');
+export const SpoutingWhale3d = wrapPng(SpoutingWhale3dPng, 'SpoutingWhale3d');
+export const Whale3d = wrapPng(Whale3dPng, 'Whale3d');
 
 export {
   BooksColor,
