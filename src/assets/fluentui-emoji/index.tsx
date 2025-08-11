39d7bb26762e6d3a8d1bf648ccd899d64c7b6e47
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-08-11 19:09:24 +0300

    ✨ feat(assets): add flat emoji assets for Fluent UI

--- src/assets/fluentui-emoji/index.tsx
+++ src/assets/fluentui-emoji/index.tsx
@@ -30,21 +30,36 @@ import SpiralShellColor from "./spiral_shell_color.svg";
 import SpoutingWhaleColor from "./spouting_whale_color.svg";
 import WhaleColor from "./whale_color.svg";
 
+import BooksFlat from "./books_flat.svg";
+import CloudFlat from "./cloud_flat.svg";
+import CrystalBallFlat from "./crystal_ball_flat.svg";
+import GameDieFlat from "./game_die_flat.svg";
+import HerbFlat from "./herb_flat.svg";
+import MemoFlat from "./memo_flat.svg";
+import PageFacingUpFlat from "./page_facing_up_flat.svg";
+import PenguinFlat from "./penguin_flat.svg";
+import RocketFlat from "./rocket_flat.svg";
+import SealFlat from "./seal_flat.svg";
+import SnakeFlat from "./snake_flat.svg";
+import SpiralShellFlat from "./spiral_shell_flat.svg";
+import SpoutingWhaleFlat from "./spouting_whale_flat.svg";
+import WhaleFlat from "./whale_flat.svg";
+
 // Export wrapped components
-export const Books3d = wrapPng(Books3dPng, 'Books3d');
-export const Cloud3d = wrapPng(Cloud3dPng, 'Cloud3d');
-export const CrystalBall3d = wrapPng(CrystalBall3dPng, 'CrystalBall3d');
-export const GameDie3d = wrapPng(GameDie3dPng, 'GameDie3d');
-export const Herb3d = wrapPng(Herb3dPng, 'Herb3d');
-export const Memo3d = wrapPng(Memo3dPng, 'Memo3d');
-export const PageFacingUp3d = wrapPng(PageFacingUp3dPng, 'PageFacingUp3d');
-export const Penguin3d = wrapPng(Penguin3dPng, 'Penguin3d');
-export const Rocket3d = wrapPng(Rocket3dPng, 'Rocket3d');
-export const Seal3d = wrapPng(Seal3dPng, 'Seal3d');
-export const Snake3d = wrapPng(Snake3dPng, 'Snake3d');
-export const SpiralShell3d = wrapPng(SpiralShell3dPng, 'SpiralShell3d');
-export const SpoutingWhale3d = wrapPng(SpoutingWhale3dPng, 'SpoutingWhale3d');
-export const Whale3d = wrapPng(Whale3dPng, 'Whale3d');
+export const Books3d = wrapPng(Books3dPng, "Books3d");
+export const Cloud3d = wrapPng(Cloud3dPng, "Cloud3d");
+export const CrystalBall3d = wrapPng(CrystalBall3dPng, "CrystalBall3d");
+export const GameDie3d = wrapPng(GameDie3dPng, "GameDie3d");
+export const Herb3d = wrapPng(Herb3dPng, "Herb3d");
+export const Memo3d = wrapPng(Memo3dPng, "Memo3d");
+export const PageFacingUp3d = wrapPng(PageFacingUp3dPng, "PageFacingUp3d");
+export const Penguin3d = wrapPng(Penguin3dPng, "Penguin3d");
+export const Rocket3d = wrapPng(Rocket3dPng, "Rocket3d");
+export const Seal3d = wrapPng(Seal3dPng, "Seal3d");
+export const Snake3d = wrapPng(Snake3dPng, "Snake3d");
+export const SpiralShell3d = wrapPng(SpiralShell3dPng, "SpiralShell3d");
+export const SpoutingWhale3d = wrapPng(SpoutingWhale3dPng, "SpoutingWhale3d");
+export const Whale3d = wrapPng(Whale3dPng, "Whale3d");
 
 export {
   BooksColor,
@@ -61,4 +76,21 @@ export {
   SpiralShellColor,
   SpoutingWhaleColor,
   WhaleColor,
+};
+
+export {
+  BooksFlat,
+  CloudFlat,
+  CrystalBallFlat,
+  GameDieFlat,
+  HerbFlat,
+  MemoFlat,
+  PageFacingUpFlat,
+  PenguinFlat,
+  RocketFlat,
+  SealFlat,
+  SnakeFlat,
+  SpiralShellFlat,
+  SpoutingWhaleFlat,
+  WhaleFlat,
 };
