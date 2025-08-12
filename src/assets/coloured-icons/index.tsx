82e047862bb84575682cb5cebc99f8723633b27b
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-08-12 11:27:20 +0300

    ✨ feat(assets): add SVG icons for social media platforms

---
+++ src/assets/coloured-icons/index.tsx
@@ -0,0 +1,7 @@
+import Bluesky from "./bluesky.svg";
+import GitHub from "./github.svg";
+import LinkedIn from "./linkedin.svg";
+import Mastodon from "./mastodon.svg";
+import X from "./x.svg";
+
+export { Bluesky, GitHub, LinkedIn, Mastodon, X };
