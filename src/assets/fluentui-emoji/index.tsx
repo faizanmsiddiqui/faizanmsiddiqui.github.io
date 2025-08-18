1e9b0b22def1d34c3e50fbb1c21a0049584b8968
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-08-18 10:03:38 +0300

    ✨ feat(emoji): add new Fluent Emoji for enhanced visuals

--- src/assets/fluentui-emoji/index.tsx
+++ src/assets/fluentui-emoji/index.tsx
@@ -5,9 +5,13 @@ import CrystalBallFlat from "./crystal_ball_flat.svg";
 import DesktopComputerFlat from "./desktop_computer_flat.svg";
 import GameDieFlat from "./game_die_flat.svg";
 import HerbFlat from "./herb_flat.svg";
+import LightBulbFlat from "./light_bulb_flat.svg";
 import MemoFlat from "./memo_flat.svg";
 import PageFacingUpFlat from "./page_facing_up_flat.svg";
 import PenguinFlat from "./penguin_flat.svg";
+import PoliceCarLightFlat from "./police_car_light_flat.svg";
+import PushpinFlat from "./pushpin_flat.svg";
+import RadioactiveFlat from "./radioactive_flat.svg";
 import RocketFlat from "./rocket_flat.svg";
 import SealFlat from "./seal_flat.svg";
 import SnakeFlat from "./snake_flat.svg";
@@ -18,6 +22,7 @@ import TechnologistFlatDefault from "./technologist_flat_default.svg";
 import TeddyBearFlat from "./teddy_bear_flat.svg";
 import TrophyFlat from "./trophy_flat.svg";
 import WhaleFlat from "./whale_flat.svg";
+import WritingHandFlatDefault from "./writing_hand_flat_default.svg";
 
 export {
   ArtistFlatDefault,
@@ -27,9 +32,13 @@ export {
   DesktopComputerFlat,
   GameDieFlat,
   HerbFlat,
+  LightBulbFlat,
   MemoFlat,
   PageFacingUpFlat,
   PenguinFlat,
+  PoliceCarLightFlat,
+  PushpinFlat,
+  RadioactiveFlat,
   RocketFlat,
   SealFlat,
   SnakeFlat,
@@ -40,4 +49,5 @@ export {
   TeddyBearFlat,
   TrophyFlat,
   WhaleFlat,
+  WritingHandFlatDefault,
 };
