af818ec8d2a2f93fdc4e0eb44be767074a9ceb9f
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-08-18 10:05:11 +0300

    ✨ feat(emoji): add new Noto Emoji for enhanced visuals

--- src/assets/noto-emoji/index.tsx
+++ src/assets/noto-emoji/index.tsx
@@ -5,9 +5,13 @@ import CrystalBall from "./emoji_u1f52e.svg";
 import DesktopComputer from "./emoji_u1f5a5.svg";
 import GameDie from "./emoji_u1f3b2.svg";
 import Herb from "./emoji_u1f33f.svg";
+import LightBulb from "./emoji_u1f4a1.svg";
 import Memo from "./emoji_u1f4dd.svg";
 import PageFacingUp from "./emoji_u1f4c4.svg";
 import Penguin from "./emoji_u1f427.svg";
+import PoliceCarLight from "./emoji_u1f6a8.svg";
+import Pushpin from "./emoji_u1f4cc.svg";
+import Radioactive from "./emoji_u2622.svg";
 import Rocket from "./emoji_u1f680.svg";
 import Seal from "./emoji_u1f9ad.svg";
 import Snake from "./emoji_u1f40d.svg";
@@ -18,6 +22,7 @@ import TechnologistDefault from "./emoji_u1f9d1_200d_1f4bb.svg";
 import TeddyBear from "./emoji_u1f9f8.svg";
 import Trophy from "./emoji_u1f3c6.svg";
 import Whale from "./emoji_u1f40b.svg";
+import WritingHandDefault from "./emoji_u270d.svg";
 
 export {
   ArtistDefault,
@@ -27,9 +32,13 @@ export {
   DesktopComputer,
   GameDie,
   Herb,
+  LightBulb,
   Memo,
   PageFacingUp,
   Penguin,
+  PoliceCarLight,
+  Pushpin,
+  Radioactive,
   Rocket,
   Seal,
   Snake,
@@ -40,4 +49,5 @@ export {
   TeddyBear,
   Trophy,
   Whale,
+  WritingHandDefault,
 };
