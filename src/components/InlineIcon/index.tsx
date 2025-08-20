5083a76333ce898ef52eeef2f17ef74d87d646d9
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-08-20 18:23:06 +0300

    ✨ feat(components): add InlineIcon component for SVG icons
    Introduced the `InlineIcon` component to facilitate the use of SVG
    icons with customizable size and style properties. This component
    allows for inline rendering of icons, enhancing the flexibility of
    icon usage throughout the application. The `mergedStyle` object
    ensures proper alignment and spacing for the icons.
    
    This change improves the overall UI consistency and makes it easier
    to integrate icons in various components.

---
+++ src/components/InlineIcon/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type InlineIconProps = {
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  size?: string | number;
+  style?: React.CSSProperties;
+};
+
+const InlineIcon: React.FC<InlineIconProps> = ({
+  Icon,
+  size = "1em",
+  style,
+}) => {
+  const mergedStyle: React.CSSProperties = {
+    display: "inline-block",
+    width: size,
+    height: size,
+    verticalAlign: "center",
+    marginRight: "0.5em",
+    marginLeft: "0.5em",
+    ...style,
+  };
+
+  return <Icon style={mergedStyle} />;
+};
+
+export default InlineIcon;
