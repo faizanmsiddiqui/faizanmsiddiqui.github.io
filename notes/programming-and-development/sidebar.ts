d8985b6cbb4f6d4b75b11d6a8311231847f2a9a5
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-09-12 16:20:16 +0300

    ✨ feat(docs): add documentation tools and sidebars
    Created new documentation pages for "Documentation Tools",
    "Programming and Development", and "Software and System Engineering".
    Each page includes a construction animation component and a sidebar
    configuration to enhance navigation.

---
+++ notes/programming-and-development/sidebar.ts
@@ -0,0 +1,13 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebar: SidebarsConfig = {
+  seriesSidebar: [
+    {
+      type: "doc",
+      label: "Programming and Development",
+      id: "index",
+    },
+  ],
+};
+
+export default sidebar;
---
+++ notes/software-and-system-engineering/sidebar.ts
@@ -0,0 +1,13 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebar: SidebarsConfig = {
+  seriesSidebar: [
+    {
+      type: "doc",
+      label: "Software and System Engineering",
+      id: "index",
+    },
+  ],
+};
+
+export default sidebar;
