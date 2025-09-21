dddee587cff78e6b3f40fdb9df424717afccbe36
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-09-21 14:23:11 +0300

    ✨ feat(emoji): add new emoji imports for enhanced visuals
    Added new emoji imports including AlienMonster, Brain, DetectiveDefault,
    GlobeWithMeridians, Robot, SatelliteAntenna, SpiderWeb, and VideoGame
    to improve the visual representation in the application. This change
    enhances the user experience by providing a wider variety of emojis
    for use throughout the platform.
    
    No breaking changes introduced.

--- src/assets/noto-emoji/index.tsx
+++ src/assets/noto-emoji/index.tsx
@@ -1,10 +1,14 @@
+import AlienMonster from "./emoji_u1f47e.svg";
 import ArtistDefault from "./emoji_u1f9d1_200d_1f3a8.svg";
 import Books from "./emoji_u1f4da.svg";
+import Brain from "./emoji_u1f9e0.svg";
 import Cloud from "./emoji_u2601.svg";
 import CrystalBall from "./emoji_u1f52e.svg";
 import DesktopComputer from "./emoji_u1f5a5.svg";
+import DetectiveDefault from "./emoji_u1f575.svg";
 import Dizzy from "./emoji_u1f4ab.svg";
 import GameDie from "./emoji_u1f3b2.svg";
+import GlobeWithMeridians from "./emoji_u1f310.svg";
 import GlowingStar from "./emoji_u1f31f.svg";
 import Herb from "./emoji_u1f33f.svg";
 import Laptop from "./emoji_u1f4bb.svg";
@@ -16,26 +20,34 @@ import PoliceCarLight from "./emoji_u1f6a8.svg";
 import Pushpin from "./emoji_u1f4cc.svg";
 import PuzzlePiece from "./emoji_u1f9e9.svg";
 import Radioactive from "./emoji_u2622.svg";
+import Robot from "./emoji_u1f916.svg";
 import Rocket from "./emoji_u1f680.svg";
+import SatelliteAntenna from "./emoji_u1f4e1.svg";
 import Seal from "./emoji_u1f9ad.svg";
 import Snake from "./emoji_u1f40d.svg";
 import Sparkles from "./emoji_u2728.svg";
+import SpiderWeb from "./emoji_u1f578.svg";
 import SpiralShell from "./emoji_u1f41a.svg";
 import SpoutingWhale from "./emoji_u1f433.svg";
 import TechnologistDefault from "./emoji_u1f9d1_200d_1f4bb.svg";
 import TeddyBear from "./emoji_u1f9f8.svg";
 import Trophy from "./emoji_u1f3c6.svg";
+import VideoGame from "./emoji_u1f3ae.svg";
 import Whale from "./emoji_u1f40b.svg";
 import WritingHandDefault from "./emoji_u270d.svg";
 
 export {
+  AlienMonster,
   ArtistDefault,
   Books,
+  Brain,
   Cloud,
   CrystalBall,
   DesktopComputer,
+  DetectiveDefault,
   Dizzy,
   GameDie,
+  GlobeWithMeridians,
   GlowingStar,
   Herb,
   Laptop,
@@ -47,15 +59,19 @@ export {
   Pushpin,
   PuzzlePiece,
   Radioactive,
+  Robot,
   Rocket,
+  SatelliteAntenna,
   Seal,
   Snake,
   Sparkles,
+  SpiderWeb,
   SpiralShell,
   SpoutingWhale,
   TechnologistDefault,
   TeddyBear,
   Trophy,
+  VideoGame,
   Whale,
   WritingHandDefault,
 };
