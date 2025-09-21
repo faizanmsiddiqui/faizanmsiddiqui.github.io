ea55c3433ad719a89f7afbeef3098a891f193cbc
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-09-22 00:46:54 +0300

    ✨ feat(emoji): add Noto emoji Unicode 16

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -3,4 +3,5 @@ export * as Devicon from "./devicon";
 export * as FluentUIEmoji from "./fluentui-emoji";
 export * as Icons8 from "./icons8";
 export * as Kit from "./kit";
+export * as NotoEmojiUnicode16 from "./noto-emoji-unicode-16";
 export * as NotoEmojiUnicode17 from "./noto-emoji-unicode-17";
