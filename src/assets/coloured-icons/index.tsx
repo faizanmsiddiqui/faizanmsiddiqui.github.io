8b513990382a438d51d38427a093ce8aab9c1452
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-09-23 16:51:43 +0300

    🔧 chore(icons): remove GitHub icon from icon set
    Deleted the GitHub icon from the coloured icons directory as it is no longer needed in the project.

--- src/assets/coloured-icons/index.tsx
+++ src/assets/coloured-icons/index.tsx
@@ -1,7 +1,6 @@
 import Bluesky from "./bluesky.svg";
-import GitHub from "./github.svg";
 import LinkedIn from "./linkedin.svg";
 import Mastodon from "./mastodon.svg";
 import X from "./x.svg";
 
-export { Bluesky, GitHub, LinkedIn, Mastodon, X };
+export { Bluesky, LinkedIn, Mastodon, X };
