58b290f0382ea8c41002be5060bb86392b191ce0
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-10-08 18:43:54 +0300

    ✨ feat(projects): populate sidebar with project documentation

--- projects/sidebar.ts
+++ projects/sidebar.ts
@@ -9,7 +9,43 @@ const sidebar: SidebarsConfig = {
         type: "doc",
         id: "index",
       },
-      items: [],
+      items: [
+        {
+          type: "doc",
+          label: "brepo",
+          id: "brepo/index",
+        },
+        {
+          type: "doc",
+          label: "centinum",
+          id: "centinum/index",
+        },
+        {
+          type: "doc",
+          label: "ipm",
+          id: "ipm/index",
+        },
+        {
+          type: "doc",
+          label: "logkey",
+          id: "logkey/index",
+        },
+        {
+          type: "doc",
+          label: "monitory",
+          id: "monitory/index",
+        },
+        {
+          type: "doc",
+          label: "nexus",
+          id: "nexus/index",
+        },
+        {
+          type: "doc",
+          label: "wonders",
+          id: "wonders/index",
+        },
+      ],
     },
   ],
 };
