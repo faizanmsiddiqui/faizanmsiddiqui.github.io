e4bb3bb7fc98ae77ac7c64360c44dc0a53d113ba
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-10-14 02:01:47 +0300

    ♻️ refactor(sidebar): comment out unused About and Resume sections
    Commented out the About and Resume categories in the sidebar configuration
    to clean up the code and improve maintainability. These sections are not
    currently in use and can be easily restored if needed in the future.

--- home/sidebar.ts
+++ home/sidebar.ts
@@ -11,24 +11,24 @@ const sidebar: SidebarsConfig = {
       },
       items: [],
     },
-    {
-      type: "category",
-      label: "About",
-      link: {
-        type: "doc",
-        id: "about/index",
-      },
-      items: [],
-    },
-    {
-      type: "category",
-      label: "Resume",
-      link: {
-        type: "doc",
-        id: "resume/index",
-      },
-      items: [],
-    },
+    // {
+    //   type: "category",
+    //   label: "About",
+    //   link: {
+    //     type: "doc",
+    //     id: "about/index",
+    //   },
+    //   items: [],
+    // },
+    // {
+    //   type: "category",
+    //   label: "Resume",
+    //   link: {
+    //     type: "doc",
+    //     id: "resume/index",
+    //   },
+    //   items: [],
+    // },
     {
       type: "category",
       label: "Socials",
