f9a514d4ef671532d00036889c0bcbe50a49a0d0
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-10-21 22:38:55 +0300

    🔧 chore(assets): rename kit to custom

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -2,6 +2,6 @@ export * as ColouredIcons from "./coloured-icons";
 export * as Devicon from "./devicon";
 export * as FluentUIEmoji from "./fluentui-emoji";
 export * as Icons8 from "./icons8";
-export * as Kit from "./kit";
+export * as Custom from "./custom";
 export * as NotoEmojiUnicode16 from "./noto-emoji-unicode-16";
 export * as NotoEmojiUnicode17 from "./noto-emoji-unicode-17";
