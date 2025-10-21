253f672c2aa22f56d649791d201dd205677f6b14
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-10-21 22:40:31 +0300

    🔧 chore(assets): remove unused Editor import

--- src/assets/custom/index.tsx
+++ src/assets/custom/index.tsx
@@ -1,5 +1,4 @@
 import Construction from "./construction.svg";
-import Editor from "./editor.svg";
 import HTTP from "./http.svg";
 
-export { Construction, Editor, HTTP };
+export { Construction, HTTP };
