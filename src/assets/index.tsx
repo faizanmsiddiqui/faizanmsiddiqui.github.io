704b4fb093467e9685053f0b3060ebb266c09806
imfsiddiqui <105414911+imfsiddiqui@users.noreply.github.com>
2025-10-21 22:42:09 +0300

    🔧 chore(assets): remove duplicate export of Custom

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,7 +1,7 @@
 export * as ColouredIcons from "./coloured-icons";
+export * as Custom from "./custom";
 export * as Devicon from "./devicon";
 export * as FluentUIEmoji from "./fluentui-emoji";
 export * as Icons8 from "./icons8";
-export * as Custom from "./custom";
 export * as NotoEmojiUnicode16 from "./noto-emoji-unicode-16";
 export * as NotoEmojiUnicode17 from "./noto-emoji-unicode-17";
